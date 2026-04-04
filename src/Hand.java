b22bfe59b419136c1777c90b644d5205f976f661
corbinhovasse-prog <corbinhovasse@gmail.com>
2026-04-03 19:31:04 -0600

    Create Hand.java
    Hand logic implemented

---
+++ src/Hand.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hand {
+    List<Card> cards = new ArrayList<>();
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int getSize() {
+        return cards.size();
+    }
+
+    public int getValue() {
+        int total = 0;
+        int aces = 0;
+
+        for (Card c : cards) {
+            total += c.value();
+            if (c.rank.equals("A")) aces++;
+        }
+
+        // drop each ace from 11 to 1 if we're busting
+        while (total > 21 && aces > 0) {
+            total -= 10;
+            aces--;
+        }
+
+        return total;
+    }
+
+    public boolean isBust() {
+        return getValue() > 21;
+    }
+
+    public boolean isBlackjack() {
+        return cards.size() == 2 && getValue() == 21;
+    }
+}
